0d5290af0a3b881d9c169c4a786e66dc8d360052
HubWorx <lukesmaul@gmail.com>
2017-12-18 15:17:36 -0800

    tidy up after finishing python course

---
+++ gmane/gline.js
@@ -0,0 +1,3 @@
+gline = [ ['Year','umich.edu','unl.edu','virginia.edu','mac.com','columbia.edu','cam.ac.uk','unicon.net','weber.edu','gmail.com','gatech.edu'],
+['2005-12',19,6,6,6,5,5,5,5,5,5]
+];
---
+++ where.js
@@ -0,0 +1,4 @@
+myData = [
+[47.32743449999999,-122.5797874, '3006 Judson St #110, Gig Harbor, WA 98335, United States'],
+[47.33234299999999,-122.5827237, '3313 Harborview Dr, Gig Harbor, WA 98332, United States']
+];
